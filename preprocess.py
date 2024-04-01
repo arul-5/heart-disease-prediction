f49bd43ffa4b80dcf244570748f7430e50a25678
Arul Kumaran P <arulkumaranp02@gmail.com>
2024-04-01 22:26:04 +0530

    added preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def preprocess_input(input_tuple):
+    # Convert input tuple to numpy array
+    input_as_numpy = np.asarray(input_tuple)
+    
+    # Reshape the array to have a single row and as many columns as elements in the original tuple
+    input_reshaped = input_as_numpy.reshape(1, -1)
+    
+    return input_reshaped
+
+def preprocess_data(data):
+    data_dict = data.dict()
+    data_dict = {key: int(value) if isinstance(value, np.int64) else value for key, value in data_dict.items()}
+    args = list(data_dict.values())
+    return args
