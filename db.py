e8f85ae25bf7d08c24a9f089a0facace07924b8d
Arul Kumaran P <arulkumaranp02@gmail.com>
2024-04-02 21:09:59 +0530

    modified for venv

--- db.py
+++ db.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 try:
-    database_url = "mongodb://localhost:27017"
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise ValueError("DATABASE_URL environment variable is not set")
     conn = MongoClient(database_url)
     print("Connected to Database")
 except Exception as e:
